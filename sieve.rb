a3b2473fff0259c21e062d2141680aa433dea8ae
Derick Gross <derick.gross@gmail.com>
2019-09-29 16:08:34 -0400

    add sieve.rb and test

---
+++ sieve.rb
@@ -0,0 +1,67 @@
+class Sieve
+  attr_reader :primes, :range, :number, :limit
+  attr_accessor :candidates
+
+  def initialize(number)
+    @number = number
+    @range = (2..number).to_a
+    @limit = Math.sqrt(number)
+    @candidates = get_candidates(range)
+    @primes = calculate_primes(range)
+  end
+
+  def calculate_primes(range)
+    i = 2
+    while i < limit
+      if candidates[i.to_s] == true
+        j = i * i
+        while j <= number
+          candidates[j.to_s] = false
+          j += i
+        end
+      end
+      i += 1
+    end
+
+    candidates.select {|_, value| value }.keys.map { |prime| prime.to_i }
+  end
+
+  def get_candidates(range)
+    c = {}
+    range.each do |x|
+      c[x.to_s] = true
+    end
+
+    c
+  end
+end
+
+# JS solution for Project Euler
+
+# function calculatePrime(value, callback) {
+#     var stringPrimes;
+#     var intPrimes;
+#     var nonPrimes = [];
+#     var candidates = {};
+#     var limit = Math.sqrt(value);
+
+#     for(var i = 2; i < value; i++) {
+#         candidates[i] = true;
+#     }
+
+#     for(var i = 2; i < limit; i++) {
+#         if(candidates[i] === true) {
+#             for(var j = i * i; j < value; j += i) {
+#                 candidates[j] = false;
+#             }
+#         }
+#     }
+#     stringPrimes = Object.keys(candidates).filter(key => candidates[key]);
+#     //intPrimes = stringPrimes.map(prime => parseInt(prime));
+
+#     if (!callback) {
+#         return stringPrimes;
+#     } else {
+#         callback(stringPrimes);
+#     }
+# }
