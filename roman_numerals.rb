bb0f594a1e0b1662a7e7bd1fc922ef2da04cb5eb
Derick Gross <derick.gross@gmail.com>
2019-09-30 16:47:50 -0400

    refactor to list adds before replacements

--- roman_numerals.rb
+++ roman_numerals.rb
@@ -5,13 +5,14 @@ class Fixnum
     (self / 1000).times { roman_numeral += "M" }
     (self % 1000 / 500).times { roman_numeral += "D" }
     (self % 1000 % 500 / 100).times { roman_numeral += "C"}
+    (self % 1000 % 500 % 100 / 10).times { roman_numeral += "X" }
+    (self % 1000 % 500 % 100 % 10).times { roman_numeral += "I" }
+
     roman_numeral.sub!("CCCCC", "D")
     roman_numeral.sub!("DCCCC", "CM")
     roman_numeral.sub!("CCCC", "CD")
-    (self % 1000 % 500 % 100 / 10).times { roman_numeral += "X" }
     roman_numeral.sub!("XXXXX", "L")
     roman_numeral.sub!("XXXX", "XL")
-    (self % 1000 % 500 % 100 % 10).times { roman_numeral += "I" }
     roman_numeral.sub!("IIIII", "V")
     roman_numeral.sub!("IIII", "IV")
     roman_numeral.sub!("LXL", "XC")
