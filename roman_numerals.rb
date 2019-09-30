04527da4fcb18b88105d6dbcbc1241dcf53909ef
Derick Gross <derick.gross@gmail.com>
2019-09-30 16:45:56 -0400

    add roman_numerals.rb and test

---
+++ roman_numerals.rb
@@ -0,0 +1,22 @@
+class Fixnum
+  def to_roman
+    roman_numeral = ""
+
+    (self / 1000).times { roman_numeral += "M" }
+    (self % 1000 / 500).times { roman_numeral += "D" }
+    (self % 1000 % 500 / 100).times { roman_numeral += "C"}
+    roman_numeral.sub!("CCCCC", "D")
+    roman_numeral.sub!("DCCCC", "CM")
+    roman_numeral.sub!("CCCC", "CD")
+    (self % 1000 % 500 % 100 / 10).times { roman_numeral += "X" }
+    roman_numeral.sub!("XXXXX", "L")
+    roman_numeral.sub!("XXXX", "XL")
+    (self % 1000 % 500 % 100 % 10).times { roman_numeral += "I" }
+    roman_numeral.sub!("IIIII", "V")
+    roman_numeral.sub!("IIII", "IV")
+    roman_numeral.sub!("LXL", "XC")
+    roman_numeral.sub!("VIV", "IX")
+
+    roman_numeral
+  end
+end
