d67bd0fce5c539a1d8d345135064ec37adc7ecbd
Derick Gross <derick.gross@gmail.com>
2019-09-29 23:12:08 -0400

    correct input validation

--- trinary.rb
+++ trinary.rb
@@ -12,7 +12,7 @@ class Trinary
 
     while trinary_parts.length > 0
       value = trinary_parts.pop
-      return 0 if value >= 8
+      return 0 if value >= 3
       return 0 if value.to_s != trinary.reverse[exponent] # exponent corresponds to index of value in reversed original trinary string
       decimal_value += value * 3**exponent
       exponent += 1
