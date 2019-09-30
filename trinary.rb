778da334ed87a0a4ecb3dc9377c6f86dc9ea1a66
Derick Gross <derick.gross@gmail.com>
2019-09-29 23:08:22 -0400

    add trinary.rb and test

---
+++ trinary.rb
@@ -0,0 +1,23 @@
+class Trinary
+  attr_reader :trinary
+
+  def initialize(trinary_string)
+    @trinary = trinary_string
+  end
+
+  def to_decimal
+    trinary_parts = trinary.split("").map(&:to_i)
+    decimal_value = 0
+    exponent = 0
+
+    while trinary_parts.length > 0
+      value = trinary_parts.pop
+      return 0 if value >= 8
+      return 0 if value.to_s != trinary.reverse[exponent] # exponent corresponds to index of value in reversed original trinary string
+      decimal_value += value * 3**exponent
+      exponent += 1
+    end
+
+    decimal_value
+  end
+end
